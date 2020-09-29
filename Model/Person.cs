edc9829a3d6c8f9b05f382a4567991c04800a9ab
Konrad <konrad@Konrads-MBP.lan>
2020-09-29 14:01:59 +0200

    Check

--- Model/Person.cs
+++ Model/Person.cs
@@ -6,6 +6,46 @@ namespace TodoIt.Model
     {
         //fields
         readonly int personId;
-        
+        string firstName;
+        public string FirstName
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(firstName))
+                {
+                throw new Exception("First name cannot be null or empty");
+                }
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+        string lastName;
+        public string LasttName
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(lastName))
+                {
+                throw new Exception("Last name cannot be null or empty");
+                }
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        //constructor
+        public Person(int personId, string firstName, string lastName)
+        {
+            this.personId = personId;
+            FirstName = firstName;
+            this.lastName = lastName;
+        }
+
     }
 }
