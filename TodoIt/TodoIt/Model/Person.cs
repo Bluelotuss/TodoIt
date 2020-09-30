ed82188e4d0c62d200a1b1f3fe5a33c210ad0867
Konrad <konrad@Konrads-MBP.lan>
2020-09-30 20:07:35 +0200

    person tested

---
+++ TodoIt/TodoIt/Model/Person.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace TodoIt
+{
+    public class Person
+    {
+        //fields
+        public int personId { get; set; }
+        string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                throw new ArgumentException("First name cannot be null or empty");
+                }
+                else
+                {
+                firstName = value;
+                }
+            }
+        }
+        string lastName;
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                throw new ArgumentException("Last name cannot be null or empty");
+                }
+                else
+                {
+                lastName = value;
+                }
+            }
+        }
+
+        //constructor
+        public Person(int personId, string firstName, string lastName)
+        {
+            this.personId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string PersonInfomation()
+        {
+            return $"Person id:{personId} | First name: {firstName}\nLast name: {lastName}\n\n";
+        }
+
+    }
+}
---
+++ TodoIt/TodoIt/Model/Todo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace TodoIt
+{
+    public class Todo
+    {
+        public int todoId { get; set; }
+        string description;
+        bool done; 
+
+    }
+}
